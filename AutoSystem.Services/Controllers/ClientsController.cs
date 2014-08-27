5e6dc2b74b134de1f80af31a5d23e0324f1e82b0
Ivo <ivo.p.petkov@gmail.com>
2014-08-27 18:11:49 +0300

    Services
    add client service fix

--- AutoSystem.Services/Controllers/ClientsController.cs
+++ AutoSystem.Services/Controllers/ClientsController.cs
@@ -1,18 +1,55 @@
-﻿using System;
+﻿using AutoSystem.DataLayer;
+using AutoSystem.Models;
+using AutoSystem.Repositories;
+using AutoSystem.Services.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace AutoSystem.Services.Controllers
 {
-    public class ClientsController : Controller
+    public class ClientsController : ApiController
     {
-        //
-        // GET: /Clients/
-        public ActionResult Index()
+        private ClientsRepository clientsRepository;
+
+        public ClientsController()
         {
-            return View();
+            var context = new AutoSystemContext();
+            this.clientsRepository = new ClientsRepository(context);
+        }
+
+        // api/clients/add
+        [HttpPost]
+        [ActionName("add")]
+        public HttpResponseMessage Add([FromBody]Client value)
+        {
+            //check for empty properties (not needed)
+            
+            //check for already registered client
+            if (clientsRepository.CheckIfClientExist(value) != null)
+            {
+                return Request.CreateResponse(HttpStatusCode.Accepted,
+                                              "Client already registered!");
+            }
+
+            
+
+            Client client = new Client()
+            {
+                ClientId = value.ClientId,
+                Name = value.Name,
+                Address = value.Address,
+                Telephone = value.Address
+            };
+
+            clientsRepository.Add(client);
+
+            return Request.CreateResponse(HttpStatusCode.Created, client);
         }
 	}
 }
