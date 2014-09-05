ceb8379f1b2afd9a48f80f3f31f6f2ecc0b4035f
Ivo <ivo.p.petkov@gmail.com>
2014-09-05 18:59:08 +0300

    get repair by id

--- AutoSystem.Services/Controllers/RepairsController.cs
+++ AutoSystem.Services/Controllers/RepairsController.cs
@@ -14,6 +14,7 @@ using System.Web.Http.ValueProviders;
 using AutoSystem.Models;
 using AutoSystem.Services.Models;
 using Forum.WebApi.Attributes;
+using System.Web.Http.Description;
 
 
 namespace AutoSystem.Services.Controllers
@@ -62,7 +63,7 @@ namespace AutoSystem.Services.Controllers
             var repairModel = new RepairModel()
             {
                 RepairId = value.RepairId,
-                Date = DateTime.Now,
+                Date = value.Date.ToString(),
                 Milage = value.Milage,
                 Price = value.Price,
                 Status = value.Status,
@@ -105,6 +106,90 @@ namespace AutoSystem.Services.Controllers
 
             return Request.CreateResponse(HttpStatusCode.BadRequest, "Invalid session key");
         }
+
+        //api/posts?keyword=web-services
+
+        //public RepairModel GetById(string id,
+        //    [ValueProvider(typeof(HeaderValueProviderFactory<string>))] string sessionKey)
+        //{
+        //    var models = this.GetAll(sessionKey)
+        //        .Where(p => p.Title.Split(Separators, StringSplitOptions.RemoveEmptyEntries).Contains(keyword));
+        //    return models;
+        //}
+
+        public RepairModel GetById(int id)
+        {
+            var repair = this.repairsRepository.Get(id);
+
+            var model = new RepairModel()
+            {
+                RepairId = repair.RepairId,
+                Status = repair.Status,
+                Date = repair.Date.ToString(),
+                Milage = repair.Milage,
+                Price = repair.Price,
+                CarId = repair.CarId,
+                PerformerId = repair.PerformerId,
+                Notes = repair.Notes,
+                Attachments = repair.Attachments
+            };
+
+            return model;
+        }
+        //// GET api/repairs/12
+        //[Route("api/repairs/{RepairId}")]
+        //[ResponseType(typeof(RepairModel))]
+        //public IHttpActionResult GetPersonsFromCompany(int id, ODataQueryOptions<Person> opts)
+        //{
+        //    IQueryable<Person> all = _peopleRepository.GetAll(p => p.Company.CompanyId == id, p => p.AllowedAction, p => p.AllowedActionNoEl, p => p.Company, p => p.AllowedActionGen).AsQueryable();
+
+        //    if (opts == null)
+        //        return Ok(all);
+
+        //    IQueryable<Person> queryResults = opts.ApplyTo(all) as IQueryable<Person>;
+        //    if (opts.InlineCount != null && opts.InlineCount.Value == InlineCountValue.AllPages)
+        //    {
+        //        JObject obj = new JObject();
+        //        obj.Add("count", queryResults.Count());
+        //        return Ok(obj);
+        //    }
+        //    else
+        //    {
+        //        return Ok(queryResults);
+        //    }
+
+        //}
+
+        //// api/repairs/all
+        //[HttpGet]
+        //[ActionName("all")]
+        //public HttpResponseMessage GetById(
+        //    [ValueProvider(typeof(HeaderValueProviderFactory<String>))] String sessionKey)
+        //{
+        //    Performer performer = performersRepository.GetBySessionKey(sessionKey);
+
+        //    if (performer != null)
+        //    {
+        //        var repairs = performer.Repairs;
+        //        var simpleRepairs = new List<SimpleRepairModel>();
+
+        //        foreach (var item in repairs)
+        //        {
+        //            var newRepair = new SimpleRepairModel()
+        //            {
+        //                RepairId = item.RepairId,
+        //                Status = item.Status,
+        //                Date = item.Date.ToString()
+        //            };
+
+        //            simpleRepairs.Add(newRepair);
+        //        }
+
+        //        return Request.CreateResponse(HttpStatusCode.OK, simpleRepairs);
+        //    }
+
+        //    return Request.CreateResponse(HttpStatusCode.BadRequest, "Invalid session key");
+        //}
 
 
 
