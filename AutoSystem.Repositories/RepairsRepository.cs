ce8054572d13b92b0c681cd1b0359ef6a22f8597
Ivo <ivo.p.petkov@gmail.com>
2014-09-10 21:46:41 +0300

    repair edit beta

--- AutoSystem.Repositories/RepairsRepository.cs
+++ AutoSystem.Repositories/RepairsRepository.cs
@@ -21,5 +21,30 @@ namespace AutoSystem.Repositories
             return dbContext.Repairs.FirstOrDefault(u => u.RepairId == repairId);
         }
 
+
+        public bool EditRepair(Repair updatedRepair, int repairId)
+        {
+            var repair = dbContext.Repairs.Find(repairId);
+            if (repair  == null)
+            {               
+                return false;                
+            }
+
+            repair.RepairId = (updatedRepair.RepairId != null) ? updatedRepair.RepairId : repair.RepairId;
+            repair.CarId = (updatedRepair.CarId != null) ? updatedRepair.CarId : repair.CarId;
+            repair.Status = (updatedRepair.Status != null) ? updatedRepair.Status : repair.Status;
+            repair.Milage = (updatedRepair.Milage != null) ? updatedRepair.Milage : repair.Milage;
+            repair.PerformerPrice = (updatedRepair.PerformerPrice != null) ? updatedRepair.PerformerPrice : repair.PerformerPrice;
+            repair.FianlePrice = (updatedRepair.FianlePrice != null) ? updatedRepair.FianlePrice : repair.FianlePrice;
+            repair.IsEditable = (updatedRepair.IsEditable != null) ? updatedRepair.IsEditable : repair.IsEditable;
+            repair.PerformerId = (updatedRepair.PerformerId != null) ? updatedRepair.PerformerId : repair.PerformerId;
+            repair.Date = (updatedRepair.Date != null) ? updatedRepair.Date : repair.Date;
+            repair.Notes = (updatedRepair.Notes != null) ? updatedRepair.Notes : repair.Notes;
+            repair.Parts = (updatedRepair.Parts != null) ? updatedRepair.Parts : repair.Parts;
+            repair.Attachments = (updatedRepair.Attachments != null) ? updatedRepair.Attachments : repair.Attachments;
+
+            dbContext.SaveChanges();
+            return true;
+        }
     }
 }
